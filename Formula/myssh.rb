5b17bf702614014f4fa3dbf98c259966488dbb5d
W Kristianto <w.kristories@gmail.com>
2025-08-15 17:36:57 +0700

    myssh 0.0.3

--- Formula/myssh.rb
+++ Formula/myssh.rb
@@ -1,12 +1,16 @@
 class Myssh < Formula
   desc "Checking for existing SSH keys"
   homepage "https://github.com/devtical/myssh"
-  url "https://github.com/devtical/myssh/releases/download/v0.0.1/myssh-x86_64-apple-darwin.tar.gz"
-  version "0.0.1"
-  sha256 "66c3a0debc369e8f3f4cccfd0bfbe87d94ae82c11fe046dd3c69aaa059830b2c"
+  url "https://github.com/devtical/myssh/releases/download/v0.0.3/myssh-x86_64-apple-darwin.tar.gz"
+  version "0.0.3"
+  sha256 "f25b60aa14eee0b4d4d9b9195348874d387d33fc6390eb8df1664cd0ffd3fd79"
   license "Apache-2.0"
 
   def install
     bin.install "myssh"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/myssh --version")
   end
 end
